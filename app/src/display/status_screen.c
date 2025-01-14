3870e0672255bbd933e09c691440eea502e6c369
Albin Karlsson <me@albinkarlsson.se>
2025-01-14 12:41:42 +0100

    added display padding parameters, fix

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -40,29 +40,29 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, ZMK_DISPLAY_PADDING_RIGHT, ZMK_DISPLAY_PADDING_TOP);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_TOP);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, ZMK_DISPLAY_PADDING_LEFT, ZMK_DISPLAY_PADDING_TOP);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, CONFIG_ZMK_DISPLAY_PADDING_LEFT, CONFIG_ZMK_DISPLAY_PADDING_TOP);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_LEFT, ZMK_DISPLAY_PADDING_LEFT, ZMK_DISPLAY_PADDING_TOP);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_LEFT, CONFIG_ZMK_DISPLAY_PADDING_LEFT, CONFIG_ZMK_DISPLAY_PADDING_TOP);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                                lv_theme_get_font_small(screen), LV_PART_MAIN);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, ZMK_DISPLAY_PADDING_LEFT, ZMK_DISPLAY_PADDING_BOTTOM);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, CONFIG_ZMK_DISPLAY_PADDING_LEFT, CONFIG_ZMK_DISPLAY_PADDING_BOTTOM);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, ZMK_DISPLAY_PADDING_RIGHT, ZMK_DISPLAY_PADDING_BOTTOM);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_BOTTOM);
 #endif
     return screen;
 }
