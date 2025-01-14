eb369b6abaf4cf1f56779528d69a5f89add96e62
Albin Karlsson <me@albinkarlsson.se>
2025-01-14 13:11:15 +0100

    added display padding parameters, fix

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -40,7 +40,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_TOP);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, -CONFIG_ZMK_DISPLAY_PADDING_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_TOP);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
@@ -62,7 +62,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_BOTTOM);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, -CONFIG_ZMK_DISPLAY_PADDING_RIGHT, CONFIG_ZMK_DISPLAY_PADDING_BOTTOM);
 #endif
     return screen;
 }
