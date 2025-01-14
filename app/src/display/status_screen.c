b49497a0d830eed20edce5d079d1aa655f917c13
Albin Karlsson <me@albinkarlsson.se>
2025-01-14 11:44:06 +0100

    added display padding parameters

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -38,24 +38,37 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
 
+    // Create a style object for padding
+    static lv_style_t style;
+    lv_style_init(&style);
+
+    // Set padding values using configuration options
+    lv_style_set_pad_top(&style, LV_STATE_DEFAULT, CONFIG_ZMK_DISPLAY_PADDING_TOP);
+    lv_style_set_pad_bottom(&style, LV_STATE_DEFAULT, CONFIG_ZMK_DISPLAY_PADDING_BOTTOM);
+    lv_style_set_pad_left(&style, LV_STATE_DEFAULT, CONFIG_ZMK_DISPLAY_PADDING_LEFT);
+    lv_style_set_pad_right(&style, LV_STATE_DEFAULT, CONFIG_ZMK_DISPLAY_PADDING_RIGHT);
+
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
+    lv_obj_add_style(zmk_widget_battery_status_obj(&battery_status_widget), LV_PART_MAIN, &style);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_add_style(zmk_widget_output_status_obj(&output_status_widget), LV_PART_MAIN, &style);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_PERIPHERAL_STATUS)
     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_LEFT, 0,
-                 0);
+    lv_obj_add_style(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_PART_MAIN, &style);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_add_style(zmk_widget_layer_status_obj(&layer_status_widget), LV_PART_MAIN, &style);
     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                                lv_theme_get_font_small(screen), LV_PART_MAIN);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
@@ -63,6 +76,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_add_style(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_PART_MAIN, &style);
     lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
 #endif
     return screen;
